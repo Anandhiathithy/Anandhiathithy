eb31c8c73264ff71b7f63d82d657581bafcfee58
Anandhiathithy <109408742+Anandhiathithy@users.noreply.github.com>
2022-12-06 10:45:48 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,27 @@
+public class Array
+{
+public static void main(String args[])
+{
+int a[]={7,5,3,6,9};
+/* Searching on element in an array
+
+
+int search=5;
+for(int i=0;i<a.length;i=i+1)
+{
+if(a[i]==search)
+{
+System.out.println("found"+search);
+}
+}
+*/
+int search=5;
+int replace=15;
+for(int i=0;i<a.length;i=i+1)
+{
+if(a[i]=replace);
+}
+System.out.println(a[]);
+
+}
+}
